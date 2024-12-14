757efca0968f2a93cdf28d4f4f80537682f139f3
caseyjbrotherton <caseyjbrotherton@gmail.com>
2024-12-14 14:10:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Howdy Bots Programming Repo
+
+Java robot code and stuff
+
+https://gist.github.com/Grimkey/0ccb4c3cac830092bf5498e9ba48e895
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class SampleTest {
+  @Test // marks this method as a test
+  public void canadd() {
+
+    assertEquals(1+1, 2);
+  }
+}
