6f716535e88f8708b20346f6269b43c2c85ece54
jameson-pi <piscitello284@gmail.com>
2025-01-07 20:28:42 -0600

    drivetrain  init

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+    public static class DriveConstants {
+        public static final int LEFT_LEADER_ID = 1;
+        public static final int LEFT_FOLLOWER_ID = 2;
+        public static final int RIGHT_LEADER_ID = 3;
+        public static final int RIGHT_FOLLOWER_ID = 4;
+        public static final int DRIVE_MOTOR_CURRENT_LIMIT = 40;
+    }
+}
