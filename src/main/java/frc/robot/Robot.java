94fa89060489bdb8af4b91abb879e1ff38ac0631
Anshu <acharya.anshu@gmail.com>
2025-01-16 19:09:18 -0800

    commit to ensure that directory is clean

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import com.ctre.phoenix.sensors.Pigeon2;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -20,6 +20,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  public static final boolean isCompetition = false;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -53,7 +55,6 @@ public class Robot extends TimedRobot {
     // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
