947608c0216b868bbcbb31af0fada13669b5dc3f
Pranav R <pranavr@howdybots.org>
2025-01-25 11:38:02 -0600

    Merge Drivebase to Main #2 (#5)
    * drivetrain  init
    
    * bugFix
    
    * just used kitbot code
    
    * add paths and fix code
    
    * make work
    
    * commit to ensure that directory is clean
    
    * REVLib Corruption Fix
    
    * Halfway finished with changes to merge Drivebase and main
    
    * Drivebase to main merge update
    
    * finish drivebase to main merge conflicts
    
    * add encoder ports to Constants
    
    * Drivebase to main merge update 2.0
    
    * Finalized Review of merge Drivebase to Main
    
    * Change to Logged Robot
    
    * Removed Useless Comments in build.gradle
    
    * Ran Spotless for Better format
    
    ---------
    
    Co-authored-by: jameson-pi <piscitello284@gmail.com>
    Co-authored-by: Anshu <acharya.anshu@gmail.com>
    Co-authored-by: ModBoyEX <79735432+ModBoyex@users.noreply.github.com>
    Co-authored-by: pranavr72 <pranavr@howdybots.org>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,75 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.subsystems.CANDriveSubsystem;
+import frc.robot.subsystems.CANRollerSubsystem;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
 public class RobotContainer {
+  // The robot's subsystems
+  private final CANDriveSubsystem driveSubsystem = new CANDriveSubsystem();
+  private final CANRollerSubsystem rollerSubsystem = new CANRollerSubsystem();
+
+  // The driver's controller
+  private final CommandXboxController driverController =
+      new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
+
+  // The operator's controller
+  private final CommandXboxController operatorController =
+      new CommandXboxController(OperatorConstants.OPERATOR_CONTROLLER_PORT);
+
+  // The autonomous chooser
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     configureBindings();
+
+    // Set the options to show up in the Dashboard for selecting auto modes. If you
+    // add additional auto modes you can add additional lines here with
+    // autoChooser.addOption
+    autoChooser.setDefaultOption("Autonomous", Autos.exampleAuto(driveSubsystem));
   }
 
-  private void configureBindings() {}
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Set input A from driver controller to run ejectCommand
+    driverController.a().onTrue(rollerSubsystem.ejectCommand());
 
+    // Set input B from driver controller to run intakeCommand
+    driverController.b().onTrue(rollerSubsystem.intakeCommand());
+
+    /// Set driveSUbystem's default Command to be arcadeDrive
+    driveSubsystem.setDefaultCommand(
+        driveSubsystem.arcadeDrive(
+            () -> -driverController.getLeftY(), () -> -driverController.getRightX()));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
